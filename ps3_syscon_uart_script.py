259fc96fa1e1a0fc61bc6987aa21e85892bea8d4
David Bentham <david.bentham@lvs.co.uk>
2021-05-11 15:59:04 +0100

    Remove whitespacing

--- ps3_syscon_uart_script.py
+++ ps3_syscon_uart_script.py
@@ -9,7 +9,7 @@ import time
 class PS3UART(object):
     ser = serial.Serial()
     type = ''
-            
+
     sc2tb = uhx('71f03f184c01c5ebc3f6a22a42ba9525')  # Syscon to TestBench Key    (0x130 xor 0x4578)
     tb2sc = uhx('907e730f4d4e0a0b7b75f030eb1d9d36')  # TestBench to Syscon Key    (0x130 xor 0x4588)
     value = uhx('3350BD7820345C29056A223BA220B323')  # 0x45B8
@@ -17,10 +17,10 @@ class PS3UART(object):
 
     auth1r_header = uhx('10100000FFFFFFFF0000000000000000')
     auth2_header  = uhx('10010000000000000000000000000000')
-    
+
     def aes_decrypt_cbc(self, key, iv, in_data):
         return AES.new(key, AES.MODE_CBC, iv).decrypt(in_data)
-    
+
     def aes_encrypt_cbc(self, key, iv, in_data):
         return AES.new(key, AES.MODE_CBC, iv).encrypt(in_data)
 
@@ -37,21 +37,21 @@ class PS3UART(object):
         self.ser.open()
         assert(self.ser.isOpen())
         self.ser.flush()
-        
+
     def __del__(self):
         self.ser.close()
-        
+
     def send(self, data):
-        self.ser.write(data.encode('ascii'))    
-                            
+        self.ser.write(data.encode('ascii'))
+
     def receive(self):
         return self.ser.read(self.ser.inWaiting())
-        
+
     def command(self, com, wait = 1, verbose = False):
         if(verbose):
             print('Command: ' + com)
-        
-        if(self.type == 'CXR'):        
+
+        if(self.type == 'CXR'):
             length = len(com)
             checksum = sum(bytearray(com, 'ascii')) % 0x100
             if(length <= 10):
@@ -63,21 +63,21 @@ class PS3UART(object):
                     self.send(com[j:j+15])
                     j += 15
                 self.send(com[j:] + '\r\n')
-        elif(self.type == 'SW'):    
+        elif(self.type == 'SW'):
             length = len(com)
             if(length >= 0x40):
                 if(self.command('SETCMDLONG FF FF')[0] != 0):
-                    return (0xFFFFFFFF, ['Setcmdlong'])        
+                    return (0xFFFFFFFF, ['Setcmdlong'])
             checksum = sum(bytearray(com, 'ascii')) % 0x100
             self.send('{}:{:02X}\r\n'.format(com, checksum))
         else:
             self.send(com + '\r\n')
-            
+
         time.sleep(wait)
         answer = self.receive().decode('ascii').strip()
         if(verbose):
             print('Answer: ' + answer)
-        
+
         if(self.type == 'CXR'):
             answer = answer.split(':')
             if(len(answer) != 3):
@@ -86,14 +86,14 @@ class PS3UART(object):
             if(answer[0] != 'R' and answer[0] != 'E'):
                 return (0xFFFFFFFF, ['Magic'])
             if(answer[1] != '{:02X}'.format(checksum)):
-                return (0xFFFFFFFF, ['Checksum'])    
+                return (0xFFFFFFFF, ['Checksum'])
             data = answer[2].split(' ')
             if(answer[0] == 'R' and len(data) < 2 or answer[0] == 'E' and len(data) != 2):
                 return (0xFFFFFFFF, ['Data length'])
             if(data[0] != 'OK' or len(data) < 2):
                 return (int(data[1], 16), [])
             else:
-                return (int(data[1], 16), data[2:])    
+                return (int(data[1], 16), data[2:])
         elif(self.type == 'SW'):
             answer = answer.split('\n')
             for i in range(0, len(answer)):
@@ -107,13 +107,13 @@ class PS3UART(object):
             ret = answer[-1][0].replace('\n', '').split(' ')
             if(len(ret) < 2 or len(ret[1]) != 8 and not all(c in string.hexdigits for c in ret[1])):
                 return (0, [x[0] for x in answer])
-            elif(len(answer) == 1):         
+            elif(len(answer) == 1):
                 return (int(ret[1], 16), ret[2:])
             else:
                 return (int(ret[1], 16), [x[0] for x in answer[:-1]])
         else:
             return (0, [answer])
-            
+
     def auth(self):
         if(self.type == 'CXR' or self.type == 'SW'):
             auth1r = self.command('AUTH1 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
@@ -160,7 +160,7 @@ class PS3UART(object):
                     return 'Auth1 response invalid'
             else:
                 return 'scopen response invalid'
-    
+
 
 def main(argc, argv):
     if(argc < 3):
@@ -185,6 +185,6 @@ def main(argc, argv):
                 print('{:08X}'.format(ret[0]) + '\n' + ''.join(ret[1]))
         else:
             print(ret[1][0])
-                
+
 if __name__ == '__main__':
     main(len(sys.argv), sys.argv)
